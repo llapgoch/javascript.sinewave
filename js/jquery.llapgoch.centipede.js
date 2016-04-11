ef54db14f84693fbdf658a5d17ac89944b74a4a5
Dave Baker <dbaker@iweb.co.uk>
2016-04-12 00:17:09 +0100

    Arc creation and drawing progress

--- js/jquery.llapgoch.centipede.js
+++ js/jquery.llapgoch.centipede.js
@@ -1,25 +1,29 @@
 jQuery.widget('llapgoch.centipede', {
     options: {
-        stageWidth: 500,
-        stageHeight: 500,
-        startPointX: 100,
-        startPointY: 100,
+        stageWidth: 800,
+        stageHeight: 700,
         minDiameter: 20,
         maxDiameter: 300,
         minDegrees: 1,
         maxDegrees: 300,
-        speed: 1
+        speed: 5,
+        arcRenderSpacing: 1,
+        drawControlPoints: true,
+        startRotation: 0,
+        startPoint:{
+            x: 100,
+            y: 100
+        }
     },
     
     canvas: null,
+    arcs:null,
     currentArc: {},
     ctx: null,
-    currentX: 0,
-    currentY: 0,
-    currentRotation: 0,
     currentCos: 0,
     currentSin: 0,
     currentDiameter: 100,
+    totalDistance: 0,
     
     _create: function(){
         var canvas =  $('<canvas />', {
@@ -29,6 +33,7 @@ jQuery.widget('llapgoch.centipede', {
         
         this.element.append(canvas);
         
+        this.arcs = [];
         this.canvas = canvas.get(0);
         this.canvas.width = this.options.stageWidth;
         this.canvas.height = this.options.stageHeight;
@@ -38,9 +43,15 @@ jQuery.widget('llapgoch.centipede', {
         this.currentX = this.options.startPointX;
         this.currentY = this.options.startPointY;
 
-        for(var i = 0; i < 20; i++){
-            this._createArc();
-        }
+        // for(var i = 0; i < 4; i++){
+ //            this._createArc(true);
+ //        }
+ 
+        this.getPosition(10000);
+        var arcPoint = this.getPointInArc(this.arcs[0], 10);
+        this._plotPoint(arcPoint, 4, 'purple');
+        
+        console.log(this.arcs);
     },
     
     _degreesToRadians: function(deg){
@@ -48,7 +59,7 @@ jQuery.widget('llapgoch.centipede', {
     },
     
     _rotatePoint: function($pPoint, $pOrigin, rot){
-        var $rp = [];
+        var $rp = {};
         var radians = (rot / 180) * Math.PI;
 
         $rp['x'] = $pOrigin['x'] + (Math.cos(radians) * ($pPoint['x'] - $pOrigin['x']) - Math.sin(radians) * ($pPoint['y'] - $pOrigin['y']));
@@ -62,86 +73,158 @@ jQuery.widget('llapgoch.centipede', {
                 (y > 0 && y < this.options.stageHeight);
     },
     
-    _createArc: function(){
-       
-        
-        var diameter,
+    _plotPoint: function(point, size, color){
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(point.x - (size / 2), point.y - (size / 2), size, size);
+        this.ctx.fill();
+    },
+    
+    _createArc: function(draw){
+        var arc,
+            endPoint,
+            diameter,
             degreeRange,
             direction,
             numberOfDegrees,
-            endRad,
             isOnStage,
-        centerPoint;
-            
+            centerPoint,
+            currentRotation,
+            startPoint,
+            circumference,
+            distance,
+            counter = 0;
         
-        var c = 0;
+        if(this.arcs.length){
+            var lastArc = this.arcs[this.arcs.length - 1];
+           
+            currentRotation = lastArc.endRotation;
+            startPoint = lastArc.endPoint;
+        }else{
+            startPoint = this.options.startPoint;
+            currentRotation = this.options.startRotation;
+        }
         
         do{
             diameter = (Math.random() * (this.options.maxDiameter - this.options.minDiameter)) + this.options.minDiameter;
             direction = Math.random() >= 0.5 ? -1 : 1;
-            numberOfDegrees = ((Math.random() * (this.options.maxDegrees - this.options.minDegrees)) + this.options.minDegrees) * direction;
+            numberOfDegrees = Math.round((Math.random() * (this.options.maxDegrees - this.options.minDegrees)) + this.options.minDegrees) * direction;
             diameter *= direction;
             
-            centerX = this.currentX + ((diameter / 2));
-            centerY = this.currentY;
+            circumference = (Math.PI * diameter);
+            // Distance of the arc to draw
+            distance = (circumference / 360) * numberOfDegrees;
+            
+            centerX = startPoint.x + ((diameter / 2));
+            centerY = startPoint.y;
             
             centerPoint = this._rotatePoint(
                 {
                     x: centerX,
                     y: centerY
                 },
-                {
-                    x: this.currentX,
-                    y: this.currentY
-                },
-                this.currentRotation
+                startPoint,
+                currentRotation
             );
             
-            endRad = this._degreesToRadians(this.currentRotation + numberOfDegrees - 180);
-            endPosY = Math.sin(endRad) * (diameter / 2) + centerPoint.y;
-            endPosX = Math.cos(endRad) * (diameter / 2) + centerPoint.x;
-            isOnStage = this._isOnStage(endPosX, endPosY);
+            arc = {
+                distance: distance,
+                totalRotation: numberOfDegrees,
+                startRotation: currentRotation,
+                endRotation: currentRotation + numberOfDegrees,
+                startPoint: startPoint,
+                centerPoint: centerPoint,
+                diameter: diameter,
+            };
             
-            c++;
+            arc.endPoint = this.getPointInArc(arc, distance);
             
-            if(c > 500){
-                console.log(endPosX, endPosY);
-                console.log("error");
+            isOnStage = this._isOnStage(arc.endPoint.x, arc.endPoint.y);
+            
+            counter++;
+            
+            // Failed to find a point within bounds
+            // Use the last one and keep going
+            if(counter > 500){
+                console.log("Too many attempts");
                 break;
             }
             
         } while(isOnStage == false);
         
         
-        var circumference = (Math.PI * diameter);
-        var distance = (circumference / 360) * numberOfDegrees;
-        var steps = distance / this.options.speed; 
-        var angleStep = numberOfDegrees / steps;
+        this.arcs.push(arc);
         
-        this.ctx.fillStyle = 'blue';
-        this.ctx.fillRect(centerPoint.x, centerPoint.y, 2, 2);
-        this.ctx.fill();
-        
-        
-        for(i = 0; i < steps; i++){
-            var val = i * angleStep;
-            
-            var rad = this._degreesToRadians(this.currentRotation + val - 180);
-            var posY = Math.sin(rad) * (diameter / 2) + centerPoint.y;
-            var posX = Math.cos(rad) * (diameter / 2) + centerPoint.x;
-
-            this.ctx.fillRect(posX, posY, 2, 2);
-            this.ctx.fill();
-            
+        if(draw){
+            this._drawArc(arc);
         }
         
-        this.ctx.fillStyle = 'red';
-        this.ctx.fillRect(this.currentX, this.currentY, 4, 4);
-        this.ctx.fill();
+        // A shorthand of the total distance of the complete shape
+        // To avoid having to always calculate via a loop
+        this.totalDistance += distance;
+    },
+    
+    getPointInArc: function(arc, position){
+        if(position > arc.distance){
+            throw 'Out of arc bounds';
+        }  
         
-        this.currentRotation += numberOfDegrees;
-        this.currentX = posX;
-        this.currentY = posY;
+        var circumference = (Math.PI * arc.diameter);
+        var angleStep = arc.totalRotation / arc.distance;
+        var val = position * angleStep;
+    
+        var rad = this._degreesToRadians(arc.startRotation + val - 180);
+        var posY = Math.sin(rad) * (arc.diameter / 2) + arc.centerPoint.y;
+        var posX = Math.cos(rad) * (arc.diameter / 2) + arc.centerPoint.x;
+        
+        return {
+            x: posX,
+            y: posY
+        };
+    },
+    
+    _drawArc: function(arc){
+         var steps = arc.distance / this.options.arcRenderSpacing; 
+        // Draws a visual representation of the path using this.options.arcRenderSpacing    
+        if(this.options.drawControlPoints){
+            this._plotPoint(arc.centerPoint, 4, 'blue');
+        }
+    
+        for(i = 0; i < steps; i++){
+               var arcPoint = this.getPointInArc(arc, i);
+               this._plotPoint(arcPoint, 2, 'red');
+        }
+        
+        if(this.options.drawControlPoints){
+            var end = this.getPointInArc(arc, arc.distance);
+            this._plotPoint(end, 6, 'green');
+       }
+    },
+    
+    // gets the position on the arcs, creates new if required
+    getPosition: function(pos){
+        var cumulativeDistance = 0,
+            currentArc,
+            relativeDistance;
+        
+        while(pos > this.totalDistance){
+            this._createArc(true);
+        }
+        
+        // TODO: Adapt this for finding the arc for the requested pos
+        for(var i = 0; i < this.arcs.length; i++){
+            currentArc = this.arcs[i];
+            
+            if(cumulativeDistance + currentArc.distance >= pos){
+                break;
+            }
+            
+            cumulativeDistance += currentArc.distance;
+        }
+        
+        relativeDistance = pos - cumulativeDistance;
+    },
+    
+    _getPositionOnArc: function(pos){
         
     }
 });
